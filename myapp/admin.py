7e8b1e19b2a744fc8bcf3fbac720ab3fad9925fe
AbbasKothari1552 <ask50405808@gmail.com>
2024-04-06 17:18:25 +0530

    Modify the Admin Area

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+# admin.site.register(Venue)
+
+# customizing the admin panel
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'address', 'phone_no')
+    ordering = ('name',)
+    search_fields = ('name', 'address', 'phone_no')
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Venue(models.Model):
+
+    name = models.CharField('Venue Name',max_length = 100)
+    address = models.CharField('Address of Venue',max_length= 300)
+    pincode = models.IntegerField('Pincode')
+    phone_no = models.IntegerField('Contact Number', blank= True)
+    website = models.CharField('Website Address',max_length = 100, blank= True)
+    email = models.EmailField('Email Address', blank= True)
+
+    def __str__(self):
+        return self.name
